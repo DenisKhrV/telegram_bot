8448d30fa7f9d948c85fe11d197fb4adb995e979
DenisKhrV <DenisKhr@yandex.ru>
2024-02-01 19:59:03 +1000

    Сделана курсовая работа “Бот в Telegram”. Сконфигурировано подключение к боту, настроена модель данных с миграцией, обработка и сохранение сообщений в базе данных и отправка напоминаний.

---
+++ src/main/java/pro/sky/telegrambot/entity/NotificationTask.java
@@ -0,0 +1,72 @@
+package pro.sky.telegrambot.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+public class NotificationTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long chatId;
+    private String text;
+    private LocalDateTime execDate;
+
+    public NotificationTask(Long chat_id, String text, LocalDateTime exec_date) {
+        this.chatId = chat_id;
+        this.text = text;
+        this.execDate = exec_date;
+    }
+
+    public NotificationTask() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDateTime getExecDate() {
+        return execDate;
+    }
+
+    public void setExecDate(LocalDateTime execDate) {
+        this.execDate = execDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NotificationTask that = (NotificationTask) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
